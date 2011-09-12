298f5fac46dbcbbf4ebaefcffcb0bb9698514a9c
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-09-12 20:17:14 +0200

    cleanups

--- chromehacking.py
+++ chromehacking.py
@@ -1,29 +1,8 @@
-# http://dev.chromium.org/developers/design-documents/appmode-mac
-
-# http://www.chromium.org/developers/design-documents
-# http://www.chromium.org/developers/design-documents/views-windowing
-# http://www.chromium.org/developers/design-documents/chromeviews
-# http://www.chromium.org/developers/design-documents/chrome-chromeos-windowing
-# http://www.chromium.org/developers/design-documents/browser-window
-
-# http://src.chromium.org/viewvc/chrome/trunk/src/chrome/browser/views/html_dialog_view.cc?revision=43137&view=markup&pathrev=43137
-# http://src.chromium.org/viewvc/chrome/trunk/src/chrome/browser/ui/cocoa/
-# http://src.chromium.org/viewvc/chrome/trunk/src/chrome/browser/ui/cocoa/themed_window.h?view=markup
-# http://src.chromium.org/viewvc/chrome/trunk/src/chrome/browser/ui/cocoa/framed_browser_window.h?view=markup
-# http://src.chromium.org/viewvc/chrome/trunk/src/chrome/browser/ui/cocoa/browser_window_controller.h?view=markup
-# http://src.chromium.org/viewvc/chrome/trunk/src/chrome/browser/ui/cocoa/browser_window_controller.mm?view=markup
-
-# http://codesearch.google.com/#lI4R5BeSKxs/ui/cocoa/applescript/browsercrapplication%2Bapplescript.mm&type=cs
-# http://codesearch.google.com/#hfE6470xZHk/chrome/browser/cocoa/applescript/tab_applescript.mm&type=cs
-
-# http://stackoverflow.com/questions/7337986/open-new-window-in-chrome-via-pyobjc-python
-
-# http://pyobjc.sourceforge.net/documentation/pyobjc-core/intro.html
+# see https://github.com/albertz/chromehacking for more background
 
 import sys, time, os, os.path
 
 import objc
-#from Foundation import NSObject
 NSObject = objc.lookUpClass("NSObject")
 NSAutoreleasePool = objc.lookUpClass("NSAutoreleasePool")
 NSScriptCommand = objc.lookUpClass("NSScriptCommand")
@@ -31,8 +10,6 @@ NSThread = objc.lookUpClass("NSThread")
 NSWindow = objc.lookUpClass("NSWindow")
 app = objc.lookUpClass("NSApplication").sharedApplication()
 _NSThemeCloseWidget = objc.lookUpClass("_NSThemeCloseWidget")
-
-#pool = NSAutoreleasePool.alloc().init()
 
 try:
 	class PyAsyncCallHelper(NSObject):
@@ -56,30 +33,7 @@ def fsbrowse(obj):
 FramedBrowserWindow = objc.lookUpClass("FramedBrowserWindow")
 BrowserWindowController = objc.lookUpClass("BrowserWindowController")
 TabStripController = objc.lookUpClass("TabStripController")
-
-# http://www.chromium.org/developers/design-documents/applescript
-# http://codesearch.google.com/#hfE6470xZHk/chrome/browser/cocoa/applescript/window_applescript.mm&type=cs
-# http://src.chromium.org/svn/trunk/src/chrome/browser/ui/cocoa/applescript/window_applescript_test.mm
 WindowAppleScript = objc.lookUpClass("WindowAppleScript")
-
-def test():
-	print >>sys.__stderr__, "Hello from main thread!"
-
-def test2():
-	#o = BrowserWindowController.alloc().initWithBrowser_(None)
-	o = WindowAppleScript.alloc().init()
-	print >>sys.stdout, "Window", o
-	return o
-
-#do_in_mainthread(test)
-#o = do_in_mainthread(test2)
-
-def replaceRunCode(ipshell):
-	runcodeattr = "run_code" if hasattr(ipshell, "run_code") else "runcode"
-	__ipython_orig_runcode = getattr(ipshell, runcodeattr)
-	def __ipython_runcode(code_obj):
-		return do_in_mainthread(lambda: __ipython_orig_runcode(code_obj))
-	setattr(ipshell, runcodeattr, __ipython_runcode)
 
 def message_via_html(title, msg, open_callback):
 	import BaseHTTPServer
@@ -113,9 +67,6 @@ def message_via_html(title, msg, open_callback):
 	return ret
 
 def isMainThread(): return NSThread.isMainThread()
-
-def browserWindows():
-	return [ w for w in app.windows() if isinstance(w, FramedBrowserWindow) ]
 
 def openPopupWindow(url):
 	assert not isMainThread()
@@ -350,4 +301,5 @@ def capi_backtrace():
 	tracePtrs = (c_void_p * N)()
 	c = capi.backtrace(addressof(tracePtrs), N)
 	capi.backtrace_symbols_fd(addressof(tracePtrs), c, sys.stdout.fileno())
-	
+
+install_webapp_handlers()
